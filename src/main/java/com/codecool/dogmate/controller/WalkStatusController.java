de64392722135dce4cf41e8aa6810005d4a27484
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-12 19:38:06 +0100

    fix typo

--- src/main/java/com/codecool/dogmate/controller/WalkStatusController.java
+++ src/main/java/com/codecool/dogmate/controller/WalkStatusController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/walkStatuss")
+@RequestMapping("/api/v1/walkStatus")
 public class WalkStatusController extends GenericController<WalkStatus, Long> {
 
     @Autowired
