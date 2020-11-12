6e00f8bab071593ad4840e4228465642259387ce
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-12 21:00:21 +0100

    add walk_statuses insert script

---
+++ src/main/resources/sql/WALK_STATUSES.sql
@@ -0,0 +1,4 @@
+insert into WALK_STATUSES(NAME) values ('PLANNED');
+insert into WALK_STATUSES(NAME) values ('CANCELLED');
+insert into WALK_STATUSES(NAME) values ('PAST');
+insert into WALK_STATUSES(NAME) values ('ONGOING');
