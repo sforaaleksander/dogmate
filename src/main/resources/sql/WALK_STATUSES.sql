238d45477d93fc523f980b5b58519d5d34371f27
sforaaleksander <sforaaleksander@gmail.com>
2020-11-13 16:53:11 +0100

    remove particular sql file inserts

--- src/main/resources/sql/WALK_STATUSES.sql
+++
@@ -1,4 +0,0 @@
-insert into WALK_STATUSES(NAME) values ('PLANNED');
-insert into WALK_STATUSES(NAME) values ('CANCELLED');
-insert into WALK_STATUSES(NAME) values ('PAST');
-insert into WALK_STATUSES(NAME) values ('ONGOING');
