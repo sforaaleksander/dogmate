896193fe56a25ad988949641af6e7811aaeb6433
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-13 13:28:39 +0100

    start implementing inserting session after successul login attempt insertion

--- src/main/java/com/codecool/dogmate/service/LoginAttemptService.java
+++ src/main/java/com/codecool/dogmate/service/LoginAttemptService.java
@@ -1,15 +1,39 @@
 package com.codecool.dogmate.service;
 
 import com.codecool.dogmate.model.LoginAttempt;
+import com.codecool.dogmate.model.Session;
 import com.codecool.dogmate.repository.LoginAttemptRepository;
+import com.codecool.dogmate.repository.SessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 @Service
 public class LoginAttemptService extends GenericService<LoginAttempt, Long> {
 
+    private SessionRepository sessionRepository;
+
     @Autowired
     LoginAttemptService(LoginAttemptRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public void insert(LoginAttempt object) {
+        super.insert(object);
+        if (object.isSuccessful()) {
+            final long duration = 1000L * 60 * 60 * 24 * 365;
+
+            Timestamp endTime = new Timestamp(object.getTime().getTime());
+            endTime.setTime(object.getTime().getTime() + duration);
+
+            sessionRepository.save(new Session("someId", object.getUser(), object.getTime(), endTime, object));
+        }
+    }
+
+    @Autowired
+    public void setSessionRepository(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
     }
 }
