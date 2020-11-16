ccfff0c1c1b176e49dc08eb0b3546b686bb3982f
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-16 14:33:10 +0100

    fix /friends/{id} get request

--- src/main/java/com/codecool/dogmate/controller/FriendsController.java
+++ src/main/java/com/codecool/dogmate/controller/FriendsController.java
@@ -1,18 +1,64 @@
 package com.codecool.dogmate.controller;
 
+import com.codecool.dogmate.exception.NotFoundException;
 import com.codecool.dogmate.model.Friends;
 import com.codecool.dogmate.model.FriendsKey;
 import com.codecool.dogmate.service.FriendsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/friends")
-public class FriendsController extends GenericController<Friends, FriendsKey> {
+public class FriendsController {
+
+    private final FriendsService friendsService;
 
     @Autowired
     FriendsController(FriendsService friendsService) {
-        super(friendsService);
+        this.friendsService = friendsService;
+    }
+
+    @GetMapping
+    public Iterable<Friends> getAll(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "id") String... sortBy
+    ) {
+        return friendsService.getAll(page, size, sortBy);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Iterable<Friends> getFriendsForUserWithId(@PathVariable Long id) {
+        return friendsService.getUserFriendsById(id);
+    }
+
+    @GetMapping(value = "/{id}/{id2}")
+    public Friends getById(@PathVariable Long id, @PathVariable Long id2) {
+        FriendsKey friendsKey = new FriendsKey(id, id2);
+        FriendsKey friendsKey2 = new FriendsKey(id2, id);
+        Friends friends = friendsService.getById(friendsKey);
+        Friends friends2 = friendsService.getById(friendsKey2);
+        if (friends == null && friends2 == null) {
+            throw new NotFoundException();
+        }
+        return friends != null ? friends : friends2;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void deleteById(@PathVariable FriendsKey id) {
+        friendsService.removeById(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    public void update(@RequestBody Friends newObject, @PathVariable FriendsKey id) {
+        friendsService.update(newObject, id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void insert(@RequestBody Friends entity) {
+        friendsService.insert(entity);
     }
 }
--- src/main/java/com/codecool/dogmate/service/FriendsService.java
+++ src/main/java/com/codecool/dogmate/service/FriendsService.java
@@ -2,15 +2,53 @@ package com.codecool.dogmate.service;
 
 import com.codecool.dogmate.model.Friends;
 import com.codecool.dogmate.model.FriendsKey;
+import com.codecool.dogmate.model.User;
 import com.codecool.dogmate.repository.FriendsRepository;
+import com.codecool.dogmate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class FriendsService extends GenericService<Friends, FriendsKey> {
 
+    private final FriendsRepository repository;
+    private final UserRepository userRepository;
+
     @Autowired
-    FriendsService(FriendsRepository repository) {
+    FriendsService(FriendsRepository repository, UserRepository userRepository) {
         super(repository);
+        this.repository = repository;
+        this.userRepository = userRepository;
+    }
+
+    public Iterable<Friends> getUserFriendsById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        Iterable<Friends> relatedUsers = Collections.emptyList();
+        Iterable<Friends> relatingUsers = Collections.emptyList();
+
+        if (optionalUser.isPresent()) {
+            relatedUsers = repository.findAllByRelatedUser(optionalUser.get());
+            relatingUsers = repository.findAllByRelatingUser(optionalUser.get());
+        }
+        Set<Friends> friends = new HashSet<>();
+        relatedUsers.forEach(u -> {
+            if (u.isAccepted()) friends.add(u);
+        });
+        relatingUsers.forEach(u -> {
+            if (u.isAccepted()) friends.add(u);
+        });
+        return friends;
+    }
+
+    @Override
+    public Friends getById(FriendsKey friendsKey) {
+        Optional<Friends> optional = repository.findById(friendsKey);
+        return optional.orElse(null);
     }
 }
