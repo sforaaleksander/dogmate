03f8b1829eb99c74f7ca7972fc06b0028bbf4163
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-17 11:04:50 +0100

    add findByEmail method

--- src/main/java/com/codecool/dogmate/repository/UserRepository.java
+++ src/main/java/com/codecool/dogmate/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends FilterActivePagingAndSortingRepository<User, Long> {
 
     Iterable<User> findAllByIsActiveTrue();
+
+    User findByEmail(String email);
 }
