11a6a8ccdecc995153e811539732918c55928392
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-18 14:32:41 +0100

    add all possible permissions

--- src/main/java/com/codecool/dogmate/security/ApplicationUserPermission.java
+++ src/main/java/com/codecool/dogmate/security/ApplicationUserPermission.java
@@ -1,10 +1,32 @@
 package com.codecool.dogmate.security;
 
 public enum ApplicationUserPermission {
-    STUDENT_READ("student:read"),
-    STUDENT_WRITE("student:write"),
-    COURSE_READ("course:read"),
-    COURSE_WRITE("course:write");
+    BREED_READ("breed:read"),
+    BREED_WRITE("breed:write"),
+    COORDINATES_READ("coordinates:read"),
+    COORDINATES_WRITE("coordinates:write"),
+    DOG_READ("dog:read"),
+    DOG_WRITE("dog:write"),
+    FRIENDS_READ("friends:read"),
+    FRIENDS_WRITE("friends:write"),
+    LOGIN_ATTEMPT_READ("login_attempt:read"),
+    LOGIN_ATTEMPT_WRITE("login_attempt:write"),
+    MESSAGE_READ("message:read"),
+    MESSAGE_WRITE("message:write"),
+    SESSION_READ("session:read"),
+    SESSION_WRITE("session:write"),
+    TEMPER_READ("temper:read"),
+    TEMPER_WRITE("temper:write"),
+    USER_READ("user:read"),
+    USER_WRITE("user:write"),
+    WALK_READ("walk:read"),
+    WALK_WRITE("walk:write"),
+    WALK_AREA_READ("walk_area:read"),
+    WALK_AREA_WRITE("walk_area:write"),
+    WALK_STATUS_READ("walk_status:read"),
+    WALK_STATUS_WRITE("walk_status:write"),
+    USER_TYPE_READ("user_type:read"),
+    USER_TYPE_WRITE("user_type:write");
 
     private final String permission;
 
