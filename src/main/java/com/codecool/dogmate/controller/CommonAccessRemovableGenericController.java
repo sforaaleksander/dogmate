fa11be1a304b4c9477dea63eb1787a23def9b876
sforaaleksander <sforaaleksander@gmail.com>
2020-11-18 16:13:13 +0100

    extract specific removal functionality to another service class

---
+++ src/main/java/com/codecool/dogmate/controller/CommonAccessRemovableGenericController.java
@@ -0,0 +1,27 @@
+package com.codecool.dogmate.controller;
+
+import com.codecool.dogmate.model.Archivable;
+import com.codecool.dogmate.model.Indexable;
+import com.codecool.dogmate.service.GenericRemovableService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class CommonAccessRemovableGenericController<T extends Indexable<ID> & Archivable, ID> extends CommonAccessGenericController<T, ID> {
+
+    private final GenericRemovableService<T, ID> service;
+
+    CommonAccessRemovableGenericController(GenericRemovableService<T, ID> service) {
+        super(service);
+        this.service = service;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER','ROLE_PREMIUM_USER')")
+    public void deleteById(@PathVariable ID id) {
+        service.removeById(id);
+    }
+}
--- src/main/java/com/codecool/dogmate/service/GenericRemovableService.java
+++ src/main/java/com/codecool/dogmate/service/GenericRemovableService.java
@@ -1,2 +1,19 @@
-package com.codecool.dogmate.service;public class GenericRemovableService {
+package com.codecool.dogmate.service;
+
+import com.codecool.dogmate.model.Archivable;
+import com.codecool.dogmate.model.Indexable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public abstract class GenericRemovableService<T extends Indexable<ID> & Archivable, ID> extends GenericService<T, ID> {
+
+    public GenericRemovableService(PagingAndSortingRepository<T, ID> repository) {
+        super(repository);
+    }
+
+    public void removeById(ID id) {
+        T t = getById(id);
+
+        t.setActive(false);
+        repository.save(t);
+    }
 }
