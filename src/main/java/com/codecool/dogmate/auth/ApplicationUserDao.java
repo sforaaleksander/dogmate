a2f830078a6a6a6e671fd84a10016e2ddc2d9e1b
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-18 12:25:02 +0100

    delete redundant classes

--- src/main/java/com/codecool/dogmate/auth/ApplicationUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.dogmate.auth;
-
-import java.util.Optional;
-
-public interface ApplicationUserDao {
-
-    Optional<ApplicationUser> selectApplicationUserByUsername(String username);
-
-}
