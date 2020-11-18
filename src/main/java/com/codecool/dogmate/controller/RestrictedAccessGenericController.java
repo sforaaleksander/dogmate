abc701dd9a46212da3c6872bb229734d450a80b2
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-18 15:53:47 +0100

    make all users access all get endpoints

--- src/main/java/com/codecool/dogmate/controller/RestrictedAccessGenericController.java
+++ src/main/java/com/codecool/dogmate/controller/RestrictedAccessGenericController.java
@@ -14,7 +14,7 @@ public abstract class RestrictedAccessGenericController<T extends Indexable<ID>,
     }
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER', 'ROLE_PREMIUM_USER')")
     public Iterable<T> getAll(
             @RequestParam(defaultValue = "0") Integer page,
             @RequestParam(defaultValue = "10") Integer size,
@@ -24,27 +24,27 @@ public abstract class RestrictedAccessGenericController<T extends Indexable<ID>,
     }
 
     @GetMapping(value = "/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER', 'ROLE_PREMIUM_USER')")
     public T getById(@PathVariable ID id) {
         return service.getById(id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(value = "/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteById(@PathVariable ID id) {
         service.removeById(id);
     }
 
     @PutMapping(value = "/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void update(@RequestBody T newObject, @PathVariable ID id) {
         service.update(newObject, id);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void insert(@RequestBody T entity) {
         service.insert(entity);
     }
