d55033e21a4519af512aa2aff44f17383b69ccf5
MateuszGolda <mateusz.golda1993@gmail.com>
2020-11-20 13:17:16 +0100

    fix dependency loop by using repository instead of service in TemperCachingFactory

--- src/main/java/com/codecool/dogmate/service/TemperCachingFactory.java
+++ src/main/java/com/codecool/dogmate/service/TemperCachingFactory.java
@@ -1,29 +1,38 @@
 package com.codecool.dogmate.service;
 
+import com.codecool.dogmate.exception.NotFoundException;
 import com.codecool.dogmate.model.Temper;
+import com.codecool.dogmate.repository.TemperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class TemperCachingFactory {
 
     private static final Map<Long, Temper> cachedTempers = new ConcurrentHashMap<>();
-    private final GenericService<Temper, Long> service;
+    private final TemperRepository repository;
     private Iterable<Temper> tempers;
 
     @Autowired
-    public TemperCachingFactory(GenericService<Temper, Long> service) {
-        this.service = service;
+    public TemperCachingFactory(TemperRepository repository) {
+        this.repository = repository;
         populateMap();
     }
 
     public Temper getTemper(Long id) {
         Temper temper = cachedTempers.get(id);
         if (temper == null) {
-            temper = service.getById(id);
+
+            Optional<Temper> optional = repository.findById(id);
+            if (optional.isPresent()) temper = optional.get();
+            else throw new NotFoundException();
+
             cachedTempers.put(id, temper);
         }
         return temper;
@@ -34,7 +43,8 @@ public class TemperCachingFactory {
     }
 
     private void populateMap() {
-        tempers = service.getAll(0, 10, new String[]{"id"});
+        tempers = repository.findAll(PageRequest.of(0, 10, Sort.by("id"))).getContent();
+
         for (Temper temper : tempers) {
             cachedTempers.put(temper.getId(), temper);
         }
