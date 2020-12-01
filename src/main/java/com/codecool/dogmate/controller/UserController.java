7937b01698537d26155d9fb40952c2c8db8f9a18
sforaaleksander <sforaaleksander@gmail.com>
2020-12-01 11:01:03 +0100

    userController overrides insert() to enable unlogged users account creation

--- src/main/java/com/codecool/dogmate/controller/UserController.java
+++ src/main/java/com/codecool/dogmate/controller/UserController.java
@@ -1,17 +1,35 @@
 package com.codecool.dogmate.controller;
 
+import com.codecool.dogmate.exception.UnprocessableEntityException;
 import com.codecool.dogmate.model.User;
 import com.codecool.dogmate.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("${api.endpoint}/users")
 public class UserController extends CommonAccessRemovableGenericController<User, Long> {
 
+    private final UserService userService;
+
     @Autowired
     UserController(UserService userService) {
         super(userService);
+        this.userService = userService;
+    }
+
+    @Override
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void insert(@RequestBody @Valid User entity, Errors errors) {
+        if (errors.hasErrors()) {
+            throw new UnprocessableEntityException();
+        }
+        userService.insert(entity);
     }
 }
