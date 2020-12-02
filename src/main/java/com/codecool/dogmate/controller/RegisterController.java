f6403cb732ce67f48ed80c1232ed07d798c15a9e
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-02 18:19:26 +0100

    implement RegisterController

---
+++ src/main/java/com/codecool/dogmate/controller/RegisterController.java
@@ -0,0 +1,32 @@
+package com.codecool.dogmate.controller;
+
+import com.codecool.dogmate.exception.UnprocessableEntityException;
+import com.codecool.dogmate.model.User;
+import com.codecool.dogmate.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+    private RegisterService registerService;
+
+    @Autowired
+    RegisterController(RegisterService registerService){
+        this.registerService = registerService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    public void insert(@RequestBody @Valid User entity, Errors errors) {
+        if (errors.hasErrors()) {
+            throw new UnprocessableEntityException();
+        }
+        registerService.insert(entity);
+    }
+
+}
