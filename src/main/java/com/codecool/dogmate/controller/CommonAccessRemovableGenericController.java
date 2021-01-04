5ca1909bee624de9e63f5a570ee69f7243fd6924
sforaaleksander <sforaaleksander@gmail.com>
2021-01-04 17:44:25 +0100

    implement patch mapping in GenericRemovableController.java

--- src/main/java/com/codecool/dogmate/controller/CommonAccessRemovableGenericController.java
+++ src/main/java/com/codecool/dogmate/controller/CommonAccessRemovableGenericController.java
@@ -5,9 +5,7 @@ import com.codecool.dogmate.model.Indexable;
 import com.codecool.dogmate.service.GenericRemovableService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 public abstract class CommonAccessRemovableGenericController<T extends Indexable<ID> & Archivable, ID> extends CommonAccessGenericController<T, ID> {
     private final GenericRemovableService<T, ID> service;
@@ -23,5 +21,19 @@ public abstract class CommonAccessRemovableGenericController<T extends Indexable
     public void deleteById(@PathVariable ID id) {
         service.removeById(id);
     }
+
+
+    @PatchMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_USER','ROLE_PREMIUM_USER')")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT, reason = "Dog partially updated")
+    public void partialUpdate(@RequestBody T updated, @PathVariable ID id) {
+        T object = service.getById(id);
+        if (object == null) {
+            return;
+        }
+        specificUpdate(object, updated);
+    }
+
+    public abstract void specificUpdate(T original, T updated);
 }
 // todo package management
