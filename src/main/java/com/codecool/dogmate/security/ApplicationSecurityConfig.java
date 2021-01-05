183196e0349b59e4f0d021b0153afeb8e07c1277
sforaaleksander <sforaaleksander@gmail.com>
2021-01-05 15:06:46 +0100

    remove unused code

--- src/main/java/com/codecool/dogmate/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codecool/dogmate/security/ApplicationSecurityConfig.java
@@ -71,7 +71,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-//        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
@@ -80,25 +79,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/api/v1/**", configuration);
         return source;
     }
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.applyPermitDefaultValues();
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-//        configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
-//
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-//        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        source.registerCorsConfiguration("/login", configuration);
-//        source.registerCorsConfiguration("/register", configuration);
-//        source.registerCorsConfiguration("/api/v1/**", configuration);
-//        return source;
-//    }
 
     @Override
     public void configure(WebSecurity web) {
